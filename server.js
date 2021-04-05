941126590357bd0985d95675a89c81dd31c07cab
rachid <rachid98baghrosse@gmail.com>
2021-04-05 13:17:02 +0200

    prepared for deployement

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
     //bring an express server
         const express = require('express'); 
         const connectDB = require('./config/db');
+        const path =require('path')
     //initialise the app variable 
         const app = express();
     // connexion to the database
@@ -11,19 +12,24 @@
         app.use(express.json({ extended : false }))  ;
            
     //create end point to test (send data to the browser)
-        app.get('/', (req, res) => res.send('API running'));
+      
 
     // define routes 
         app.use('/api/users', require('./routes/api/users'));
         app.use('/api/profile', require('./routes/api/profile'));
         app.use('/api/posts', require('./routes/api/posts'));
         app.use('/api/auth', require('./routes/api/auth'));
-        // app.use((req, res, next) => {
-        //     res.setHeader('Access-Control-Allow-Origin', '*');
-        //     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-        //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-        //     next();
-        //   });
+        
+        // serve static assets in production
+
+        if (process.env.NODE_ENV === 'production') {
+            //set satatic folder
+            app.use(express.static('client/build'))
+
+            app.get('*',(req,res)=>{
+                res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+            })
+        }
 
     //create a port
     //usefull when we will wnat to deploy it in heroko but locally we will use the port 5000
